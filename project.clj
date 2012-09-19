294f40504fe8557a65896d3c6da9a02e5fffe60d
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-09-19 11:35:38 +0700

    use another ver of gzip middleware that works

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject bootstrap-docs "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]
+                           [noir "1.3.0-beta10"]
                            [bootstrap "0.0.1-SNAPSHOT"]
-                           [rm-hull/ring-gzip-middleware "0.1.3"]]
-            :dev-dependencies [[appengine-magic "0.5.0"]]
+                           [amalloy/ring-gzip-middleware "0.1.2"]]
             :main bootstrap-docs.server)
