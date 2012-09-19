b49adfe0d139715883148c6693b12f31d656c9e3
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-09-19 10:50:45 +0700

    add gzip middleware

--- src/bootstrap_docs/server.clj
+++ src/bootstrap_docs/server.clj
@@ -1,7 +1,10 @@
 (ns bootstrap-docs.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server])
+  (:use [ring.middleware.gzip :only [wrap-gzip]]))
 
 (server/load-views-ns "bootstrap-docs.controllers")
+
+(server/add-middleware wrap-gzip)
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
